7d940adbc3e0636a5fcd0ccca9256763fc86ed99
AntonKolomiiets <anton.kolomiiets@logz.io>
2023-08-28 20:29:59 +0300

    add test fix

--- src/utils/formatDate.test.ts
+++ src/utils/formatDate.test.ts
@@ -4,6 +4,7 @@ describe('formatDate', () => {
     it('should format a date string correctly', () => {
         const inputDate = '2023-08-28';
         const formattedDate = formatDate(inputDate);
+
         expect(formattedDate).toBe('2023-8-28');
     });
 });
